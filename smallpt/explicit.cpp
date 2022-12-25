b3bee708920f9153c5d8303ae3a3e621acfa3187
Paleshell <zhangzheng0928@163.com>
2022-12-25 21:59:15 +0800

    fix most

--- smallpt/explicit.cpp
+++ smallpt/explicit.cpp
@@ -73,6 +73,31 @@ struct Vec_avx
         return Vec_avx(_mm256_blendv_ps(x, b.x, mask), _mm256_blendv_ps(y, b.y, mask), _mm256_blendv_ps(z, b.z, mask));
     }
 };
+struct Vec_avxd
+{
+    __m256d x, y, z;
+    Vec_avxd(__m256d x_ = _mm256_setzero_pd(), __m256d y_ = _mm256_setzero_pd(), __m256d z_ = _mm256_setzero_pd()) : x(x_), y(y_), z(z_) {}
+    Vec_avxd(const Vecd &v) : x(_mm256_set1_pd(v.x)), y(_mm256_set1_pd(v.y)), z(_mm256_set1_pd(v.z)) {}
+    Vec_avxd(const Vec_avx &v, int start) : x(_mm256_cvtps_pd(_mm256_extractf128_ps(v.x, start))),
+                                            y(_mm256_cvtps_pd(_mm256_extractf128_ps(v.y, start))),
+                                            z(_mm256_cvtps_pd(_mm256_extractf128_ps(v.z, start))) {}
+    Vec_avxd operator+(const Vec_avxd &b) const { return Vec_avxd(x + b.x, y + b.y, z + b.z); }
+    Vec_avxd operator-(const Vec_avxd &b) const { return Vec_avxd(x - b.x, y - b.y, z - b.z); }
+    Vec_avxd operator*(__m256d b) const { return Vec_avxd(x * b, y * b, z * b); }
+    Vec_avxd operator/(__m256d b) const { return Vec_avxd(x / b, y / b, z / b); }
+    Vec_avxd mult(const Vec_avxd &b) const { return Vec_avxd(x * b.x, y * b.y, z * b.z); }
+    Vec_avxd &norm()
+    {
+        __m256d dist = _mm256_sqrt_pd(x * x + y * y + z * z);
+        return *this = *this / dist;
+    }
+    __m256d dot(const Vec_avxd &b) const { return x * b.x + y * b.y + z * b.z; } // cross
+    Vec_avxd operator%(Vec_avxd &b) { return Vec_avxd(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
+    Vec_avxd blend(const Vec_avxd &b, __m256d mask) const
+    {
+        return Vec_avxd(_mm256_blendv_pd(x, b.x, mask), _mm256_blendv_pd(y, b.y, mask), _mm256_blendv_pd(z, b.z, mask));
+    }
+};
 struct Ray
 {
     Vec o, d;
@@ -83,6 +108,11 @@ struct Ray_avx
 {
     Vec_avx o, d;
     Ray_avx(Vec_avx o_, Vec_avx d_) : o(o_), d(d_) {}
+};
+struct Ray_avxd
+{
+    Vec_avxd o, d;
+    Ray_avxd(Vec_avxd o_, Vec_avxd d_) : o(o_), d(d_) {}
 };
 enum Refl_t
 {
@@ -98,8 +128,8 @@ struct Sphere
     Sphere(float rad_, Vec p_, Vec e_, Vec c_, Refl_t refl_) : rad(rad_), p(p_), e(e_), c(c_), refl(refl_) {}
     float intersect(const Ray &r) const
     {                     // returns distance, 0 if nohit
-        Vec op = Vec(p) - r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-        float t, eps = 1e-4, b = op.dot(r.d), det = b * b - op.dot(op) + rad * rad;
+        Vecd op = Vecd(p) - r.o; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+        double t, eps = 1e-4, b = op.dot(r.d), det = b * b - op.dot(op) + rad * rad;
         if (det < 0)
             return 0;
         else
@@ -108,14 +138,21 @@ struct Sphere
     }
     __m256 intersect_avx(const Ray_avx &r) const
     {
-        Vec_avx op = Vec_avx(p) - r.o;
-        __m256 t, eps = _mm256_set1_ps(1e-4), b = op.dot(r.d), det = b * b - op.dot(op) + _mm256_set1_ps(rad * rad);
-        __m256 mask = _mm256_cmp_ps(det, _mm256_setzero_ps(), _CMP_LT_OQ);
-        det = _mm256_sqrt_ps(det);
-        __m256 mask2 = _mm256_cmp_ps(b - det, eps, _CMP_GT_OQ);
-        __m256 mask3 = _mm256_cmp_ps(b + det, eps, _CMP_GT_OQ);
-        __m256 ans = _mm256_blendv_ps(_mm256_blendv_ps(_mm256_setzero_ps(), b + det, mask3), b - det, mask2);
-        return _mm256_blendv_ps(ans, _mm256_setzero_ps(), mask);
+        __m256 ret;
+        for (int i = 0; i < 2; i++)
+        {
+            Vec_avxd od(r.o, i), dd(r.d, i);
+            Vec_avxd op = Vec_avxd(p) - od; // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
+            __m256d t, eps = _mm256_set1_pd(1e-4), b = op.dot(dd), det = b * b - op.dot(op) + _mm256_set1_pd(rad * rad);
+            __m256d mask = _mm256_cmp_pd(det, _mm256_setzero_pd(), _CMP_LT_OQ);
+            det = _mm256_sqrt_pd(det);
+            __m256d mask2 = _mm256_cmp_pd(b - det, eps, _CMP_GT_OQ);
+            __m256d mask3 = _mm256_cmp_pd(b + det, eps, _CMP_GT_OQ);
+            __m256d ans = _mm256_blendv_pd(_mm256_blendv_pd(_mm256_setzero_pd(), b + det, mask3), b - det, mask2);
+            ans = _mm256_blendv_pd(ans, _mm256_setzero_pd(), mask);
+            ret = _mm256_insertf128_ps(ret, _mm256_cvtpd_ps(ans), i);
+        }
+        return ret;
     }
 };
 Sphere spheres[] = {
@@ -262,6 +299,91 @@ inline void erand48tri(unsigned short *Xi, __m256 &c, __m256 &s)
 }
 #define CHKMASK(mask) debug(__LINE__, depth, mask)
 // AVX version
+/*
+const int MAX_DEPTH = 100;
+struct saveinfo
+{
+    Vec_avx pre, f, ans;
+    __m256 mask;
+} s[MAX_DEPTH];
+Vec_avx radiance_avx(Ray_avx r, __m256 mask, saveinfo s[], unsigned short *Xi, int E = 1)
+{
+    int depth = 0;
+    for (;;)
+    {
+        Vec_avx ans;
+        if (_mm256_testz_ps(mask, mask))
+            break;
+        __m256 t;                      // distance to intersection
+        __m256 id = _mm256_set1_ps(0); // id of intersected object
+        mask = _mm256_and_ps(mask, intersect_avx(r, t, id));
+        // for mask[i] == 0, the corresponding value in ans is not updated
+        Sphere_avx obj(id); // the hit objects
+        Vec_avx x = r.o + r.d * t;
+        Vec_avx n = (x - obj.p).norm();
+        Vec_avx nl = n.blend(n * _mm256_set1_ps(-1), _mm256_cmp_ps(r.d.dot(n), _mm256_setzero_ps(), _CMP_LT_OQ));
+        Vec_avx f = obj.c;
+        __m256 p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y
+                                                            : f.z; // max refl
+        __m256 mask2 = _mm256_and_ps(mask, _mm256_or_ps((__m256)_mm256_set1_epi32(++depth > 5 ? 0xffffffff : 0), _mm256_cmp_ps(p, _mm256_set1_ps(0), _CMP_EQ_OQ)));
+        __m256 mask3 = _mm256_cmp_ps(erand48v(Xi), p, _CMP_LT_OQ);
+        f = f.blend(f * (1.0 / p), _mm256_and_ps(mask2, mask3));
+        ans = ans.blend(obj.e * _mm256_set1_ps(E), _mm256_andnot_ps(mask3, mask2));
+        mask = _mm256_andnot_ps(_mm256_andnot_ps(mask3, mask2), mask);
+        __m256 mask4 = _mm256_cmp_ps(obj.refl, _mm256_set1_ps(0), _CMP_EQ_OQ); // Ideal DIFFUSE reflection
+        // __m256 r1 = erand48v(Xi) * 2 * M_PI;
+        __m256 r2 = erand48v(Xi), r2s = _mm256_sqrt_ps(r2);
+        Vec_avx w = nl;
+        __m256 mask5 = _mm256_or_ps(_mm256_cmp_ps(w.x, _mm256_set1_ps(0.1), _CMP_GT_OQ), _mm256_cmp_ps(w.x, _mm256_set1_ps(-0.1), _CMP_LT_OQ));
+        Vec_avx u = (Vec_avx(_mm256_set1_ps(1)).blend(Vec_avx(_mm256_set1_ps(0), _mm256_set1_ps(1)), mask5) % w).norm();
+        Vec_avx v = w % u;
+        __m256 r1c, r1s;
+        erand48tri(Xi, r1c, r1s);
+        Vec_avx d = (u * r1c * r2s + v * r1s * r2s + w * _mm256_sqrt_ps(1 - r2)).norm();
+
+        // Loop over any lights
+        Vec_avx e;
+        for (int i = 0; i < numSpheres; i++)
+        {
+            const Sphere &s = spheres[i];
+            if (s.e.x <= 0 && s.e.y <= 0 && s.e.z <= 0)
+                continue; // skip non-lights
+
+            Vec_avx sw = Vec_avx(s.p) - x;
+            __m256 mask6 = _mm256_or_ps(_mm256_cmp_ps(sw.x, _mm256_set1_ps(0.1), _CMP_GT_OQ), _mm256_cmp_ps(sw.x, _mm256_set1_ps(-0.1), _CMP_LT_OQ));
+            Vec_avx su = (Vec_avx(_mm256_set1_ps(1)).blend(Vec_avx(_mm256_set1_ps(0), _mm256_set1_ps(1)), mask6) % sw).norm();
+            Vec_avx sv = sw % su;
+            __m256 cos_a_max = _mm256_sqrt_ps(1 - s.rad * s.rad / (x - Vec_avx(s.p)).dot(x - Vec_avx(s.p)));
+            __m256 eps1 = erand48v(Xi);
+            __m256 cos_a = 1 - eps1 + eps1 * cos_a_max;
+            __m256 sin_a = _mm256_sqrt_ps(1 - cos_a * cos_a);
+            // __m256 phi = 2 * M_PI * erand48v(Xi);
+            __m256 phic, phis;
+            erand48tri(Xi, phic, phis);
+            Vec_avx l = su * phic * sin_a + sv * phis * sin_a + sw * cos_a;
+            l.norm();
+            __m256 t1, id1;
+            __m256 isect = intersect_avx(Ray_avx(x, l), t1, id1);
+            __m256 mask7 = _mm256_and_ps(isect, _mm256_cmp_ps(id1, _mm256_set1_ps(i), _CMP_EQ_OQ));
+            __m256 omega = 2 * (float)M_PI * (1 - cos_a_max);
+            e = e.blend(e + f.mult(Vec_avx(s.e) * l.dot(nl) * omega) * _mm256_set1_ps(M_1_PI), mask7);
+        }
+        // ans = ans.blend(obj.e * _mm256_set1_ps(E) + e + f.mult(radiance_avx(Ray_avx(x, d), _mm256_and_ps(mask4, mask), depth, Xi)), _mm256_and_ps(mask4, mask));
+        mask = _mm256_and_ps(mask4, mask);
+        s[depth].ans = ans;
+        s[depth].mask = mask;
+        s[depth].pre = obj.e * _mm256_set1_ps(E) + e;
+        s[depth].f = f;
+        r = Ray_avx(x, d);
+    }
+    Vec_avx ans;
+    for (; depth; depth--)
+    {
+        ans = s[depth].ans.blend(s[depth].pre + s[depth].f.mult(ans), s[depth].mask);
+    }
+    return ans;
+}
+*/
 Vec_avx radiance_avx(const Ray_avx &r, __m256 mask, int depth, unsigned short *Xi, int E = 1)
 {
     Vec_avx ans;
@@ -274,7 +396,7 @@ Vec_avx radiance_avx(const Ray_avx &r, __m256 mask, int depth, unsigned short *X
     Sphere_avx obj(id); // the hit objects
     Vec_avx x = r.o + r.d * t;
     Vec_avx n = (x - obj.p).norm();
-    Vec_avx nl = n.blend(n * _mm256_set1_ps(-1), _mm256_cmp_ps(r.d.dot(n), _mm256_setzero_ps(), _CMP_LT_OQ));
+    Vec_avx nl = n.blend(n * _mm256_set1_ps(-1), _mm256_cmp_ps(r.d.dot(n), _mm256_setzero_ps(), _CMP_GE_OQ));
     Vec_avx f = obj.c;
     __m256 p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y
                                                         : f.z; // max refl
@@ -327,18 +449,52 @@ Vec_avx radiance_avx(const Ray_avx &r, __m256 mask, int depth, unsigned short *X
     }
 
     /* compare answer with reference
-    Vec_avx ans_ref;
     for (int i = 0; i < 8; i++)
     {
-        int idx = id[i];
-        Sphere obj = spheres[idx];
-        if (obj.refl == DIFF)
+        float t_ref;
+        int id_ref;
+        Ray r_ref(Vec(r.o.x[i], r.o.y[i], r.o.z[i]), Vec(r.d.x[i], r.d.y[i], r.d.z[i]));
+        if (!intersect(r_ref, t_ref, id_ref))
         {
-            Vec w_ref(nl.x[i], nl.y[i], nl.z[i]);
+            if (mask[i])
+            {
+                if (ans.x[i] != 0 || ans.y[i] != 0 || ans.z[i] != 0)
+                {
+                    printf("i=%d, ans=(%f,%f,%f)\n", i, ans.x[i], ans.y[i], ans.z[i]);
+                    exit(1);
+                }
+            }
+            continue;
+        }
+        const Sphere &obj_ref = spheres[id_ref];
+        Vec x_ref = r_ref.o + r_ref.d * t_ref;
+        Vec n_ref = (x_ref - obj_ref.p).norm();
+        Vec nl_ref = n_ref.dot(r_ref.d) < 0 ? n_ref : n_ref * -1;
+        Vec f_ref = obj_ref.c;
+        float p_ref = f_ref.x > f_ref.y && f_ref.x > f_ref.z ? f_ref.x : f_ref.y > f_ref.z ? f_ref.y
+                                                                                           : f_ref.z; // max refl
+        if (depth > 5 || !p_ref)
+            if (rnd[i] < p_ref)
+                f_ref = f_ref * (1 / p_ref);
+            else
+            {
+                Vec ans_ref = obj_ref.e * E;
+                if (mask[i])
+                {
+                    if (fabs((ans_ref.x - ans.x[i]) / ans_ref.x) > eps || fabs((ans_ref.y - ans.y[i]) / ans_ref.y) > eps || fabs((ans_ref.z - ans.z[i]) / ans_ref.z) > eps)
+                    {
+                        printf("i=%d, ans=(%f,%f,%f), ans_ref=(%f,%f,%f)\n", i, ans.x[i], ans.y[i], ans.z[i], ans_ref.x, ans_ref.y, ans_ref.z);
+                        printf("p_ref=%f, p=%f\n", p_ref, p[i]);
+                        exit(1);
+                    }
+                }
+            }
+        if (obj_ref.refl == DIFF)
+        {
+            Vec w_ref = nl_ref;
             Vec u_ref = ((fabs(w_ref.x) > .1 ? Vec(0, 1) : Vec(1)) % w_ref).norm();
             Vec v_ref = w_ref % u_ref;
             Vec d_ref = (u_ref * r1c[i] * r2s[i] + v_ref * r1s[i] * r2s[i] + w_ref * sqrt(1 - r2[i])).norm();
-            Vec x_ref = Vec(x.x[i], x.y[i], x.z[i]);
             Vec e_ref;
             for (int j = 0; j < numSpheres; j++)
             {
@@ -360,7 +516,6 @@ Vec_avx radiance_avx(const Ray_avx &r, __m256 mask, int depth, unsigned short *X
                 if (intersect(Ray(x_ref, l), t_ref, id_ref) && id_ref == j)
                 {
                     float omega_ref = 2 * (float)M_PI * (1 - cos_a_max_ref);
-                    Vec f_ref(f.x[i], f.y[i], f.z[i]);
                     e_ref = e_ref + f_ref.mult(s_ref.e * l.dot(w_ref) * omega_ref) * M_1_PI;
                 }
             }
@@ -368,12 +523,16 @@ Vec_avx radiance_avx(const Ray_avx &r, __m256 mask, int depth, unsigned short *X
             {
                 if (fabs((e_ref.x - e.x[i]) / e_ref.x) > eps || fabs((e_ref.y - e.y[i]) / e_ref.y) > eps || fabs((e_ref.z - e.z[i]) / e_ref.z) > eps)
                 {
-                    printf("i=%d, e_ref=(%f, %f, %f), e=(%f, %f, %f)\n", i, e_ref.x, e_ref.y, e_ref.z, e.x[i], e.y[i], e.z[i]);
+                    printf("i=%d, e=(%f,%f,%f), e_ref=(%f,%f,%f)\n", i, e.x[i], e.y[i], e.z[i], e_ref.x, e_ref.y, e_ref.z);
+                    printf("obj.p=(%f,%f,%f), obj_ref.p=(%f,%f,%f)\n", obj.p.x[i], obj.p.y[i], obj.p.z[i], obj_ref.p.x, obj_ref.p.y, obj_ref.p.z);
+                    printf("id=%f, id_ref=%d\n", id[i], id_ref);
+                    printf("t=%f, t_ref=%f\n", t[i], t_ref);
                     exit(1);
                 }
             }
         }
     }
+    // puts("done");
     */
 
     ans = ans.blend(obj.e * _mm256_set1_ps(E) + e + f.mult(radiance_avx(Ray_avx(x, d), _mm256_and_ps(mask4, mask), depth, Xi)), _mm256_and_ps(mask4, mask));
